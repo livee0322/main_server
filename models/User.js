47833b57a1a198fb096cd9802a3a623352ef9b41
livee0322 <dlxodnd6564@naver.com>
2025-08-05 02:55:37 +0900

    User.js 업데이트

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  name: { type: String, required: true }, // ✅ 이름 추가
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
