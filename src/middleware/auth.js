c57c080d0d4b0e7e308e89a08b0e6b37b905f9e2
livee0322 <dlxodnd6564@naver.com>
2025-08-08 00:25:22 +0900

    auth.js 업데이트

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // 이후 req.user로 사용자 정보 접근 가능
+    req.user = decoded.user; // ✅ user.id 접근 가능
     next();
   } catch (err) {
     return res.status(403).json({ message: "유효하지 않은 토큰입니다." });
